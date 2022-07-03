99a0ac47a693b164a1059514dc667b9423ea4792
Claudioalvesdev <claudioalvesdev@gmail.com>
2022-07-03 16:15:52 -0300

    Update 03.07

--- bissecao.py
+++ bissecao.py
@@ -15,22 +15,35 @@ Max = 100
 c = b - a
 
 x0 = (a + b) / 2.0
+antB = 0
+antA = 0
 
 while(c > l or math.fabs(f(x0)) > tol):
 
     if(f(a) * f(x0) < 0.0):
         b = x0
+        erro = antB - b
+        antB = b
+        print("b: %f" % b)
 
     if(f(a) * f(x0) > 0.0):
         a = x0
+        erro = antA - a
+        antA = a
+        print("a: %f" % a)
+
 
     c = b - a
     x0 = (a + b) / 2.0
+    print("Xn: %f" % x0)
+    print("f(x): %f" % c)
 
     conte = conte + 1
+    print("d: %d\n\n" % conte)
+    #print("erro: %f\n\n" % erro)
     if(conte >= Max):
         break
 
 print("\n\n\nRaiz: %f\n" % x0)
 print("Interações %i\n" % conte)
-print("f(%f) = %f" %(x0, f(x0)))
+print("f(%f) = %f" % (x0, f(x0)))
